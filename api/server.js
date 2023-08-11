82e6bf64375501dcbfdfa93d3d0947d501648619
Rao Hammad Ali <108279711+RaoSahab14127@users.noreply.github.com>
2023-08-12 00:08:19 +0500

    done

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
-console.log("starter")
+import express from "express"; 
+import mongoose from "mongoose";
+
+const app = express()
+try {
+    await mongoose.connect('mongodb+srv://eb20103109raohammadali:14127rao@cluster0.m8dnmic.mongodb.net/?retryWrites=true&w=majority&dbname=ThirdJob');
+} catch (error) {
+    console.log(error);
+}
+
+app.listen(8000,()=>{
+    console.log("Server is running")
+} )
